d3ae3c508b118b021c8b1b8ec1a6806b01fce6b4
Tinuv <1772825289@qq.com>
2019-03-19 20:24:36 +0800

    commit

---
+++ script.ps1
@@ -0,0 +1,6 @@
+echo "Git 快速提交脚本";
+cd "D:\\My Bolg\\tinuv.github.io";
+git add .;
+git commit -a -m "commit";
+git pull origin master;
+git push origin master;
--- 快速提交.cmd
+++
@@ -1,6 +0,0 @@
-echo "====快速提交脚本===="
-d:
-cd d://my bolg/tinuv.github.io
-git add .
-git commit -a -m "提交"
-git pull origin master
